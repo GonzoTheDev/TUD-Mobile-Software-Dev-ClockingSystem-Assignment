8f9dc5446a6c938fc96db5e63c3262f29636ad2b
GonzoTheDev <c20703429@mytudublin.ie>
2022-11-27 03:25:24 +0000

    Update Shift.java

--- app/src/main/java/com/C20703429/clockingsystem/Shift.java
+++ app/src/main/java/com/C20703429/clockingsystem/Shift.java
@@ -1,9 +1,40 @@
 package com.C20703429.clockingsystem;
 
 public class Shift {
-    public Integer ID;
-    public Integer employeeID;
-    public java.sql.Date Date;
-    public java.sql.Time startTime;
-    public java.sql.Time endTime;
+    private Integer ID;
+    private Employee employee;
+    private java.sql.Date Date;
+    private java.sql.Time startTime;
+    private java.sql.Time endTime;
+
+    public Integer getID() {
+        return ID;
+    }
+    public void setID(Integer iD) {
+        ID = iD;
+    }
+    public Employee getEmployee() {
+        return employee;
+    }
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+    public java.sql.Date getDate() {
+        return Date;
+    }
+    public void setDate(java.sql.Date date) {
+        Date = date;
+    }
+    public java.sql.Time getStartTime() {
+        return startTime;
+    }
+    public void setStartTime(java.sql.Time startTime) {
+        this.startTime = startTime;
+    }
+    public java.sql.Time getEndTime() {
+        return endTime;
+    }
+    public void setEndTime(java.sql.Time endTime) {
+        this.endTime = endTime;
+    }
 }
